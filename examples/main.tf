c1baa59d6c68fc6b68b02eab307896461e6c23e6
gari199 <luis-lim@MacArra-20.local>
2024-06-26 19:13:56 +0200

    vpc name

--- examples/main.tf
+++ examples/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 module "vpc" {
   source                       = "/../"
   region                       = "eu-central-1"
-  project_name                 = "my-vpc"
+  project_name                 = "module-project"
   vpc_cidr                     = "10.0.0.0/16"
   public_subnet_az1_cidr       = "10.0.0.0/24"
   public_subnet_az2_cidr       = "10.0.1.0/24"
